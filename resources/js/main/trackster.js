4e0bc80add60d168d686e506764013f1b3407723
devrok <roland.kurz@gmx.at>
2018-03-09 23:25:08 +0100

    loading seems to be ok

--- resources/js/main/trackster.js
+++ resources/js/main/trackster.js
@@ -20,7 +20,35 @@ var Trackster = {};
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.
 */
-Trackster.renderTracks = function(tracks) {
+Trackster.renderTracks = function(trackmatches) {
+  //console.log(tracks);
+  // var x = tracks.length;
+  // console.log(x);
+  // ab hier passt was nicht
+  // console.log(tracks.length);
+  var tracks = trackmatches.track;
+
+  for (var i = 0; i < tracks.length; i++) {
+
+    var number = i;
+    var trackName = tracks[i].name;
+    var artist = tracks[i].artist;
+    var albumArt =tracks[i].image[1]["#text"];
+    var listener =tracks[i].listeners;
+    var length = "n/A";
+
+    var trackRow = "<div class=\"row result-container-row align-items-center\">"
+      +"<i class=\"col-md-auto offset-md-1 fa fa-play-circle-o fa-lg\"></i>"
+      +"<span class=\"col-md-auto\">" + number +"</span>"
+      +"<span class=\"col-md-3\">" + trackName +"</span>"
+      +"<span class=\"col-md-2\">" + artist + "</span>"
+      +"<img class=\"col-md-1\" src=\"" + albumArt +"\" />"
+      +"<span class=\"col-md-1\">" + listener +"</span>"
+      +"<span class=\"col-md-1\">" + length +"</span>"
+      +"</div>";
+
+    $("#result-container").append(trackRow);
+  }
 
 };
 
@@ -35,7 +63,8 @@ Trackster.searchTracksByTitle = function(title) {
     url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + title +"&api_key=" + API_KEY +"&format=json",
     datatype: "jsonp",
     success: function(data) {
-      console.log(data);
+      //console.log(data);
+      Trackster.renderTracks(data.results.trackmatches);
     }
   });
 };
